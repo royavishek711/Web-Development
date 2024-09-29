90db7c8024f02d8d97edc221d3cb84e4af3d21ec
Avishek roy <royavishek711@gmail.com>
2024-09-29 18:35:01 +0530

    js commit

---
+++ JavaScript/script.js
@@ -0,0 +1,34 @@
+// console.log('Hello JS')
+
+// let s='Hello'
+// s+=8
+// console.log(s+8, typeof(s))
+// let num='55'
+// num+=6
+// console.log(num+9, typeof(num))
+// let n=4
+// // n+='hi'
+// // n+='6'
+// console.log(n+'4', typeof(n))
+
+// const a=4 //can't change this later
+// // a=6 //throws error
+// console.log(a)
+
+// const obj={
+//     name:'Avishek',
+//     id:'4',
+//     dept:'DevOps',
+//     salary:'40000'
+// }
+
+// obj.newkey='newvalue'
+// obj.newkey2='newvalu23'
+// obj.newkey3='newvalue3'
+
+// obj.salary='50000'
+
+// console.log(obj, typeof(obj))
+
+
+
